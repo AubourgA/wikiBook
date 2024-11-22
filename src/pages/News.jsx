bfc4aa05c11de32c4ff625ac0a51b7aada76b2be
Adrien <adrien.aubourg@hotmail.fr>
2024-11-22 15:50:52 +0100

    fix: text content

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,7 +4,7 @@ import Title from '../components/ui/Title';
 export default function News() {
   return (
     <div>
-        <Title text1="Notre Actualités" custom1='py-2' />
+        <Title text1="Notre Actualité" custom1='py-2' />
 
         <p className="px-4">Pas de nouvelles actuellement</p>
     </div>
